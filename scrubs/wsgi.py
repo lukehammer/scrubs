3a836860a8a3fdc948fa9645ed1ee90e9a8c8da7
Ubuntu <ubuntu@ip-172-31-45-34.us-west-2.compute.internal>
2015-04-27 04:46:50 +0000

    first commit

---
+++ scrubs/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for scrubs project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scrubs.settings")
+
+application = get_wsgi_application()
